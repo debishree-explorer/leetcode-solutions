83716480e8abf583a7200378ce7582f2c5e71a06
debishree-explorer <debishree1833@gmail.com>
2026-04-16 21:55:30 +0530

    Time: 5 ms (79.7%), Space: 46.1 MB (39.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;int sum = 0;
+        if(x<0) return false;
+        while(x!=0){
+            int rem = x%10;
+            sum = sum*10+rem;
+            x /=10;
+        }
+        return sum == temp ;
+    }
+}
