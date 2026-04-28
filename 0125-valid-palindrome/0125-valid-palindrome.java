51e49612a653c891e9752b60af8d0435fce0935b
debishree-explorer <debishree1833@gmail.com>
2026-04-28 16:00:26 +0530

    Time: 254 ms (5.16%), Space: 53.4 MB (5.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,31 +1,20 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+    public boolean isPalindrome(String str) {
 
-        int left = 0;
-        int right = s.length() - 1;
+        String s=str.toLowerCase();
+        String alpha="";
+        String rev="";
 
-        while (left < right) {
+         for(int i=0;i<s.length();i++){
+        if((s.charAt(i)>='a'&&s.charAt(i)<='z')||s.charAt(i)>='0'&&s.charAt(i)<='9'){
 
-            // skip non-alphanumeric from left
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            // skip non-alphanumeric from right
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            // compare (case-insensitive)
-            if (Character.toLowerCase(s.charAt(left)) != 
-                Character.toLowerCase(s.charAt(right))) {
-                return false;
-            }
-
-            left++;
-            right--;
+            alpha=alpha+s.charAt(i);
+            rev=s.charAt(i)+rev;
         }
+         }
+         if(alpha.equals(rev)) return true;
 
-        return true;
+         return false;
+
     }
 }
